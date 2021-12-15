054780dcf10b9e560cff9cc5c0773705aa773054
riazarri <80068549+riazarri@users.noreply.github.com>
2021-12-15 23:48:48 +0200

    Update PerceptronElderTraveller.java

--- src/PerceptronElderTraveller.java
+++ src/PerceptronElderTraveller.java
@@ -1,21 +1,40 @@
-public class PerceptronElderTraveller implements PerceptronTraveller{
+package Towns;
+
+public class PerceptronElderTraveller implements PerceptronTraveller {
+	// age 60-115
 	private int age;
 
-	//age 60-115
-	public  PerceptronElderTraveller(int age) {
-		this.setAge(age);
-		
-	//orizo hardcoded ta dianismata weights kai 1 bias
+	public PerceptronElderTraveller() {
+		super();
 
+	}
+
+	public PerceptronElderTraveller(int age) {
+		this.setAge(age);
+
+	}
+
+	double array_vector[] = { 1, 1, 0, 0, 1, 1, 1, 0.23, 0.33, 0.11, 0 };// 10 features (+) bias=0
+	
+	public static boolean recommend() {
+		return false;
+
+	}
+
+	public double[] getArray_vector() {
+		return array_vector;
+	}
+
+	public void setArray_vector(double[] array_vector) {
+		this.array_vector = array_vector;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	public int getAge() {
 		return age;
 	}
 
-	public void setAge(int age) {
-		this.age = age;
-	}
-	
-	
 }
