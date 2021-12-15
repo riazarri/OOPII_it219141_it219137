8f8fa471d2d7cacd40ab523729afb1d837b12e2d
riazarri <80068549+riazarri@users.noreply.github.com>
2021-12-15 23:40:09 +0200

    Update PerceptronMiddleTraveller.java

--- src/PerceptronMiddleTraveller.java
+++ src/PerceptronMiddleTraveller.java
@@ -1,14 +1,31 @@
+package Towns;
+
 public class PerceptronMiddleTraveller implements PerceptronTraveller {
-//age 25-60
+	// age 25-60
 	private int age;
 
-	//Array Vector = {0 , 0, 1}
-	//ArrayList
-	//orizo hardcoded ta dianismata weights kai 1 bias
-	
-	public 	PerceptronMiddleTraveller(int age) {
+	public PerceptronMiddleTraveller(int age) {
 		this.setAge(age);
-		
+
+	}
+
+	public PerceptronMiddleTraveller() {
+		super();
+	}
+
+	double array_vector[] = { 1, 1, 1, 0, 1, 1, 1, 0.88, 0.70, 0.45, 1 };// 10 features (+) bias =1
+
+	public double[] getArray_vector() {
+		return array_vector;
+	}
+
+	public void setArray_vector(double[] array_vector) {
+		this.array_vector = array_vector;
+	}
+    
+	public static boolean recommend() {
+		return false;
+
 	}
 
 	public int getAge() {
@@ -18,39 +35,4 @@ public class PerceptronMiddleTraveller implements PerceptronTraveller {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
-	
-	
-	/*	public int MiddleAge;	
-//age 25-60
-	@Override
-	double calculate_similarity(City obj) {
-		double sumA=0,sumB=0,sumC=0;
-		
-		for(int i=0 ; i<10 ; i++) {
-			sumA = sumA + (obj.getTerm(i)*(i));
-			sumB = sumB + Math.pow(obj.getTerm(i),2);
-			sumC = sumC + Math.pow(getTerm(i), 2);
-		}
-		
-		if(sumB == 0 || sumC == 0) {
-			return 0;
-		}
-		return sumA/(Math.sqrt(sumB)*Math.sqrt(sumC));
-	}*/
-
-	
-	
-	
-	
-	
-//}
